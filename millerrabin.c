ded917ed65f63ad7dfd36b7970034920ebc429d4
y-arjun-y <arjyadav7@gmail.com>
2021-12-01 20:34:27 +0400

    added gitignore and formatted file

--- millerrabin.c
+++ millerrabin.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /* 
  * calculates (a * b) % c taking into account that a * b might overflow 
  */
+
 long long mulmod(long long a, long long b, long long mod)
 {
-    long long x = 0,y = a % mod;
+    long long x = 0, y = a % mod;
     while (b > 0)
     {
         if (b % 2 == 1)
-        {    
+        {
             x = (x + y) % mod;
         }
         y = (y * 2) % mod;
@@ -34,24 +36,24 @@ long long modulo(long long base, long long exponent, long long mod)
     }
     return x % mod;
 }
- 
+
 /*
  * Miller-Rabin Primality test, iteration signifies the accuracy
  */
-int Miller(long long p,int iteration)
+int Miller(long long p, int iteration)
 {
- 
+
     int i;
     long long s;
     if (p < 2)
     {
         return 0;
     }
-    if (p != 2 && p % 2==0)
+    if (p != 2 && p % 2 == 0)
     {
         return 0;
     }
-     s = p - 1;
+    s = p - 1;
     while (s % 2 == 0)
     {
         s /= 2;
@@ -79,7 +81,7 @@ int main()
     long long num;
     printf("Enter integer to test primality: ");
     scanf("%lld", &num);
-    if ( Miller( num, iteration))
+    if (Miller(num, iteration))
         printf("\n%lld is prime\n", num);
     else
         printf("\n%lld is not prime\n", num);
