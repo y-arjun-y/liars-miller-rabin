c467e84882978a70ad8b3a676c5626764ff9e3e3
Arjun Yadav <58625220+y-arjun-y@users.noreply.github.com>
2021-12-08 14:58:51 +0400

    removed variables

--- len_composite.py
+++ len_composite.py
@@ -4,10 +4,6 @@ Returns the length of composite numbers of a given range.
 
 from miller_rabin import miller_rabin
 
-upto = 100
-witness = 2
-
-
 def len_composite(int, a):
     results = []
 
@@ -16,6 +12,3 @@ def len_composite(int, a):
             results.append(miller_rabin(i, a))
 
     return len(results)
-
-
-print(len_composite(upto, witness))
