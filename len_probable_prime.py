38cbefbe2e17036f783ef026bedcb668dd4e487a
Arjun Yadav <58625220+y-arjun-y@users.noreply.github.com>
2021-12-08 14:58:36 +0400

    removed variables

--- len_probable_prime.py
+++ len_probable_prime.py
@@ -4,10 +4,6 @@ Returns the length of prime numbers of a given range.
 
 from miller_rabin import miller_rabin
 
-upto = 100
-witness = 2
-
-
 def len_probable_primes(int, a):
     results = []
 
@@ -16,6 +12,3 @@ def len_probable_primes(int, a):
             results.append(miller_rabin(i, a))
 
     return len(results)
-
-
-print(len_probable_primes(upto, witness))
