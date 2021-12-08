51bb7bed45eea3fc5a771330ca37cc2a2570e047
Arjun Yadav <58625220+y-arjun-y@users.noreply.github.com>
2021-12-08 14:58:17 +0400

    removed variables

--- probable_prime.py
+++ probable_prime.py
@@ -4,10 +4,6 @@ Returns the probable prime numbers of a given range.
 
 from miller_rabin import miller_rabin
 
-upto = 100
-witness = 2
-
-
 def probable_primes(int, a):
     results = []
 
@@ -16,6 +12,3 @@ def probable_primes(int, a):
             results.append(i)
 
     return results
-
-
-print(probable_primes(upto, witness))
